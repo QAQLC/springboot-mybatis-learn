a3fbee150621888ea113054029f1eb570b548a96
QAQLC <873228767@qq.com>
2023-05-06 18:46:23 +0800

    pom 依赖处理

--- springboot-mybatis/src/main/java/com/example/pojo/User.java
+++ springboot-mybatis/src/main/java/com/example/pojo/User.java
@@ -1,5 +1,13 @@
 package com.example.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
     private Integer id;
     private String user_name;
@@ -7,76 +15,4 @@ public class User {
     private Short gender;
     private String create_time;
     private String update_time;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Short getGender() {
-        return gender;
-    }
-
-    public void setGender(Short gender) {
-        this.gender = gender;
-    }
-
-    public String getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(String create_time) {
-        this.create_time = create_time;
-    }
-
-    public String getUpdate_time() {
-        return update_time;
-    }
-
-    public void setUpdate_time(String update_time) {
-        this.update_time = update_time;
-    }
-
-    public User(Integer id, String user_name, String name, Short gender, String create_time, String update_time) {
-        this.id = id;
-        this.user_name = user_name;
-        this.name = name;
-        this.gender = gender;
-        this.create_time = create_time;
-        this.update_time = update_time;
-    }
-
-    public User() {
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", user_name='" + user_name + '\'' +
-                ", name='" + name + '\'' +
-                ", gender=" + gender +
-                ", create_time='" + create_time + '\'' +
-                ", update_time='" + update_time + '\'' +
-                '}';
-    }
 }
